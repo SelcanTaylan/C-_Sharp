f7c65139067eea169b4e29ed43c9261be6d76f63
Selcan <selcantaylan@gmail.com>
2024-12-19 16:35:25 +0300

    Add files via upload

---
+++ 19.12.2024/ödev1/ödev1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ödev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEPosta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstEPosta = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEPosta
+            // 
+            this.txtEPosta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtEPosta.Location = new System.Drawing.Point(255, 52);
+            this.txtEPosta.Name = "txtEPosta";
+            this.txtEPosta.Size = new System.Drawing.Size(160, 35);
+            this.txtEPosta.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "E-Posta Adresinizi Giriniz:";
+            // 
+            // lstEPosta
+            // 
+            this.lstEPosta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lstEPosta.FormattingEnabled = true;
+            this.lstEPosta.ItemHeight = 22;
+            this.lstEPosta.Location = new System.Drawing.Point(443, 12);
+            this.lstEPosta.Name = "lstEPosta";
+            this.lstEPosta.Size = new System.Drawing.Size(239, 268);
+            this.lstEPosta.TabIndex = 2;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnEkle.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEkle.Location = new System.Drawing.Point(194, 109);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(211, 44);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnEkle;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(716, 317);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lstEPosta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEPosta);
+            this.Font = new System.Drawing.Font("MV Boli", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEPosta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstEPosta;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ 19.12.2024/ödev1/ödev1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string eposta = txtEPosta.Text;
+            // TextBox'tan girilen e-posta adresini alır ve 'eposta' değişkenine atar.
+
+            if (string.IsNullOrEmpty(eposta)) // E-posta alanı boş veya null ise
+            {
+                MessageBox.Show("Lütfen E-Posta Alanını Boş Bırakmayınız!!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Kullanıcıya uyarı mesajı gösterir.
+            }
+            else
+            {
+                if (eposta.Contains('@') && eposta.Contains('.')) // E-posta '@' ve '.' karakterlerini içeriyorsa
+                {
+                    int indexno = eposta.IndexOf('@');
+                    // E-posta adresinde '@' karakterinin pozisyonunu belirler ve 'indexno' değişkenine atar.
+
+                    if (eposta.Substring(0, indexno).Length >= 6)
+                    {
+                        // '@' karakterinden önceki kısmın uzunluğunun en az 6 karakter olup olmadığını kontrol eder.
+
+                        if (eposta.IndexOf('.', indexno) > indexno)
+                        {
+                            lstEPosta.Items.Add(eposta.Substring(0, indexno));
+                            // '@' karakterinden önceki kısmı ListBox'a ekler.
+                        }
+                        else
+                        {
+                            MessageBox.Show("@ İşaretinden Sonra '.' Olmalıdır!!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            // '@' karakterinden sonra '.' karakteri yoksa, kullanıcıya uyarı mesajı gösterir.
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("E-Posta Adresiniz En Az 6 Karakterden Oluşmalıdır!!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        // '@' karakterinden önceki kısım en az 6 karakter değilse, kullanıcıya uyarı mesajı gösterir.
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("E-Posta Adresinizi Kontrol Ediniz!!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    // E-posta adresinde '@' ve '.' karakterleri yoksa, kullanıcıya uyarı mesajı gösterir.
+                }
+            }
+
+
+        }
+    
+
+    }
+}
